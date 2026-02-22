0aae4534ee1f804ef4c587df67bb6b0964cf5e66
Sapna2025 <sapna2006saini@gmail.com>
2026-02-22 15:54:54 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+//console.log("Hello World");
+// document.write("Hello World");
+var num=10;
+console.log(num);
+console.log(typeof(num));
+num=false;
+console.log(num);
+console.log(typeof(num));
+num="Sanjeev Soni";
+console.log(num);
+console.log(typeof(num));
+num=null;
+console.log(num);
+console.log(typeof(num));
+num=undefined;
+console.log(num);
+console.log(typeof(num));
